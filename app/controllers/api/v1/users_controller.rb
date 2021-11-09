597f48dbad2bb603eba43da48d7cc9a8351672b9
mlgvla <mlgerardvla@gmail.com>
2021-11-08 23:16:59 -0500

    fixed formatting

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
         render json: UserSerializer.new(users)
     end
 
-    def create
+    def create    
         user = User.new(user_params)
         if user.save
             render json: UserSerializer.new(user), status: :accepted
