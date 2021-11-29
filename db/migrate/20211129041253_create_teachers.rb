5b4baa6b7447515670ec50e31b9d821f73f8be23
mlgvla <mlgerardvla@gmail.com>
2021-11-28 23:15:33 -0500

    Added Teacher model/controller/table

---
+++ db/migrate/20211129041253_create_teachers.rb
@@ -0,0 +1,12 @@
+class CreateTeachers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
