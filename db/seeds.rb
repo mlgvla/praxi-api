3496a3bac843e8d1553ede646b1523f37c5f4a31
mlgvla <mlgerardvla@gmail.com>
2021-11-30 00:17:05 -0500

    Added new tables to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create([ {username: "mlgvla", email: "monica@test.com", password: "swordfish", role: "student" }])
+users = User.create([ {username: "mlgvla", email: "monica@test.com", password_digest: "swordfish", role: "student" }])
 
 categories = Category.create([{name: "Scales"}, {name: "Etudes"}, {name: "Repertoire"}, {name: "Chamber Music"}, {name: "Orchestra Excerpts"}])
 
 instruments = Instrument.create([{name: "Violin"}, {name: "Viola"}, {name: "Cello"}, {name: "Contrabass"}, {name: "Piano"}])
+
+teachers = Teacher.create([{name: "Monica Gerard", username: "vlamlg", email: "mlg@test.com", password_digest: "haddock"}, {name: "Karen Ritscher", username: "karenviola", email: "kr@test.com", password_digest: "catfish"}])
+
+students = Student.create([{teacher_id: 1, instrument_id: 2, name: "Mason Lee", username: "masonvla", email: "mason@test.com", password_digest: "salmon"}, {teacher_id: 2, instrument_id:1 , name: "Ashley Wu", username: "ashleyvln", email: "ashley@test.com", password_digest: "goldfish"} ])
+
+tasks = Task.create([{student_id: 1, category_id: 1 , date: "2021-11-21" , title: "3-octave C Major Scale" , notes: "Practiced in rhythms" }, {student_id: 1, category_id: 2, date: "2021-11-21" , title: "Dounis Daily Dozen #8, #9, #10", notes: "Worked on articulation"}])
+
+assignments = Assignment.create({student_id: 1 , date: "2021-11-21" , tasks: [1,2], notes: "Make sure you focus on steady rhythm"})
